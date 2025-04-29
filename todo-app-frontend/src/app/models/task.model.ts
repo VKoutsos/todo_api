3051d515b99040f89b1fac6de015bbe3ec3e1b4b
VKoutsos <koutsosbilly@gmail.com>
2025-04-29 11:54:37 +0300

    added subtask component in tasks

--- todo-app-frontend/src/app/models/task.model.ts
+++ todo-app-frontend/src/app/models/task.model.ts
@@ -1,7 +1,10 @@
+import {Subtask} from './subtask.model';
+
 export interface Task{
   id:number;
   title:string;
   description:string;
   status:'pending'|'completed';
   created_at?:string;
+  subtasks?:Subtask[];
 }
