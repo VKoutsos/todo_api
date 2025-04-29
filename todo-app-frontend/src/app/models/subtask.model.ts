ff5b189e84f3beeaeeac703ff52df6fc21c8f28a
VKoutsos <koutsosbilly@gmail.com>
2025-04-29 11:48:50 +0300

    subtask service and model

---
+++ todo-app-frontend/src/app/models/subtask.model.ts
@@ -0,0 +1,8 @@
+export interface Subtask{
+  id:number;
+  task_id:number;
+  title:string;
+  description:string;
+  status:'pending'|'completed';
+  created_at?:string;
+}
---
+++ todo-app-frontend/src/app/services/subtask.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Subtask } from '../models/subtask.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubtaskService {
+  private apiUrl='http://localhost:5000/subtasks';
+
+  constructor(private http:HttpClient) { }
+
+  getSubtasks(taskId:number,token:string): Observable<Subtask[]> {
+    const headers=new HttpHeaders({'Authorization': `Bearer ${token}`});
+    return this.http.get<Subtask[]>(`${this.apiUrl}/${taskId}`,{headers});
+  }
+
+  createSubtask(taskId:number,subtask:Partial<Subtask>,token:string):Observable<any>{
+    const headers=new HttpHeaders({'Authorization':`Bearer ${token}`});
+    return this.http.post(`${this.apiUrl}/create/${taskId}`,subtask, { headers });
+  }
+
+  updateSubtask(subtaskId:number,subtask:Partial<Subtask>,token:string):Observable<any>{
+    const headers=new HttpHeaders({'Authorization': `Bearer ${token}`});
+    return this.http.put(`${this.apiUrl}/update/${subtaskId}`,subtask, { headers });
+  }
+
+  deleteTask(subtaskId:number,token:string):Observable<any>{
+    const headers=new HttpHeaders({'Authorization': `Bearer ${token}`});
+    return this.http.delete(`${this.apiUrl}/delete/${subtaskId}`,{headers});
+  }
+}
