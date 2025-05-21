016ffecc52b8f33cebbc157161e8036e4b6a04f3
VKoutsos <koutsosbilly@gmail.com>
2025-05-21 15:08:19 +0300

    logout toast and color fix

--- todo-app-frontend/src/app/navbar/navbar.component.ts
+++ todo-app-frontend/src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { ToastService } from '../services/toast.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,7 +10,9 @@ import { Router } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  constructor(public authService: AuthService, private router: Router){}
+  constructor(public authService: AuthService,
+              private router: Router,
+              private toastService: ToastService) { }
 
   get isAdmin():boolean{
     return this.authService.isAdmin();
@@ -17,6 +20,7 @@ export class NavbarComponent {
 
   logout():void{
     this.authService.logout();
+    this.toastService.showSuccess('User logged out.');
     this.router.navigate(['/login']);
   }
 }
