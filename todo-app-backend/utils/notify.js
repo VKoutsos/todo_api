badd7cb1659d40fe6f7d03139d0781f4fd413862
VKoutsos <koutsosbilly@gmail.com>
2025-06-11 21:11:01 +0300

    sending to user console log hidden

--- todo-app-backend/utils/notify.js
+++ todo-app-backend/utils/notify.js
@@ -6,12 +6,12 @@ const ADMIN_EMAIL=process.env.EMAIL_USER;
 module.exports=function notify(userEmail,subject,message){
     //send to user (if denied)
     if (userEmail){
-        console.log("Sending to user:",userEmail);
+        /*console.log("Sending to user:",userEmail);*/
         sendMail(userEmail,subject,message);
     }
 
     if (ADMIN_EMAIL && userEmail!==ADMIN_EMAIL){
-        console.log("Sending to admin:",ADMIN_EMAIL);
+        /*console.log("Sending to admin:",ADMIN_EMAIL);*/
         sendMail(ADMIN_EMAIL,subject,message);
     }
 };
